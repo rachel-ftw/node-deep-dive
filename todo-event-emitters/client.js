76f1cb20fa2223c24a4814243f77834acc58bbba
Rachel Ralston <rachelralston@gmail.com>
2017-03-19 16:52:55 -0700

    complete simple event emitter to do list, set up for socket chat room

--- todo-event-emitters/client.js
+++ todo-event-emitters/client.js
@@ -8,3 +8,14 @@ const rl = readLine.createInterface({
 
 const client = new EventEmitter()
 const server = require('./server')(client)
+server.on('response', response => {
+  process.stdout.write('\u001B[2J\u001B[0;0f')
+  process.stdout.write(response)
+  process.stdout.write('\n\> ')
+})
+
+let command, args
+rl.on('line', (input) => {
+  [command, ...args] = input.split(' ')
+  client.emit('command', command, args)
+})
--- todo-event-emitters/server.js
+++ todo-event-emitters/server.js
@@ -1,7 +1,60 @@
 const EventEmitter = require('events')
 
 class Server extends EventEmitter {
+  constructor(client) {
+    super()
+    this.tasks = {}
+    this.taskId = 1
 
+    process.nextTick(() => {
+      this.emit(
+        'response', 
+        'Type a command below (help to list available commands)')
+    })
+
+    client.on('command', (command, args) => {
+      console.log(`you've commanded: ${command}`)
+      switch(command) {
+        case 'help':
+        case 'add':
+        case 'ls':
+        case 'delete':
+          this[command](args)
+          break
+        default:
+          this.emit('response', 'Unknown Command...')
+      }
+    })
+  }
+
+  tasksString() {
+    return Object.keys(this.tasks).map(key => {
+      return `${key}: ${this.tasks[key]}`
+    }).join('\n')
+  }
+
+  help() {
+    this.emit('response', `Available Commands:
+      add task
+      ls
+      delete :id`
+     )
+  }
+
+  add(args) {
+    this.tasks[this.taskId] = args.join(' ')
+    this.emit('response', `Added task ${this.taskId}`)
+    this.taskId++
+  }
+
+  ls() {
+    this.emit('response', `Tasks:\n${this.tasksString()}`)
+  }
+
+  delete(args) {
+    delete(this.tasks[args[0]])
+    this.emit('response', `Deleted task ${args[0]}`)
+  }
 }
 
-module.exports = new Server();
+module.exports = (client) => new Server(client);
